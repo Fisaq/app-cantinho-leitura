087640f5c89b237a153cb6b7d6884dd3db7047c1
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2025-01-14 14:21:55 -0300

    Create GerenteLogin.js

---
+++ src/screens/pathsAdm/GerenteLogin.js
@@ -0,0 +1,23 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function GerenteLogin(){
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}> Tela de Login Gerente</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create(
+  {
+    container:{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text:{
+      fontSize: 24,
+      fontWeight: 'bold',
+    }
+  }
+);
